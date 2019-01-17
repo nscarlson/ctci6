5f068ab3c6afd6d4ba2f1cb43cdbd5579924f7a5
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-01-17 16:16:25 -0700

    chapter 02

---
+++ chapter02/chapter02.test.js
@@ -0,0 +1,51 @@
+const chapter02 = require('./chapter02')
+
+describe('Chapter 02', () => {
+    describe('deleteMiddleNode', () => {
+        it('returns correct results', () => {
+            expect(chapter02.deleteMiddleNode()).toEqual()
+        })
+    })
+
+    describe('intersection', () => {
+        it('returns correct results', () => {
+            expect(chapter02.intersection()).toEqual()
+        })
+    })
+
+    describe('loopDetection', () => {
+        it('returns correct results', () => {
+            expect(chapter02.loopDetection()).toEqual()
+        })
+    })
+
+    describe('palindrome', () => {
+        it('returns correct results', () => {
+            expect(chapter02.palindrome()).toEqual()
+        })
+    })
+
+    describe('partition', () => {
+        it('returns correct results', () => {
+            expect(chapter02.partition()).toEqual()
+        })
+    })
+
+    describe('removeDups', () => {
+        it('returns correct results', () => {
+            expect(chapter02.removeDups()).toEqual()
+        })
+    })
+
+    describe('returnKthToLast', () => {
+        it('returns correct results', () => {
+            expect(chapter02.returnKthToLast()).toEqual()
+        })
+    })
+
+    describe('sumLists', () => {
+        it('returns correct results', () => {
+            expect(chapter02.sumLists()).toEqual()
+        })
+    })
+})
