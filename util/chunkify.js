9f4a23bd77995834b5ec5b856e490b148047d065
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-02-02 11:50:48 -0700

    clean up chunkify

--- util/chunkify.js
+++ util/chunkify.js
@@ -1,5 +1,5 @@
 /**
- * Provided an array, splits an array into n chunks as evenly as possible
+ * Provided an array, splits an array into n chunks as balanced as possible
  * 
  * @param {number[]}    arr An array of numbers
  * @param {number}      n   Integer number of chunks
@@ -7,28 +7,31 @@
  * @returns {number[number[]]} An array of chunks from original array
  */
 const chunkify = (arr, n) => {
-    // Only one element, return arr
-    if (arr.length < 2) {
-        return arr
-    }
-
     const length = arr.length
     const result = []
     let groupSize
 
-    // Balanced case
+    // Only one element, return arr
+    if (length < 2) {
+        return arr
+    }
+
+    // Even case
     if (length % n === 0) {
         groupSize = Math.floor(length / n)
 
         for (let i = 0; i < length; i += groupSize) {
             result.push(arr.slice(i, i + groupSize))
         }
-    } else {
-        for (let i = 0; i < length; i += groupSize, n--) {
-            groupSize = Math.ceil((length - i) / n)
 
-            result.push(arr.slice(i, i + groupSize))
-        }
+        return result
+    }
+    
+    // Uneven case
+    for (let i = 0; i < length; i += groupSize, n--) {
+        groupSize = Math.ceil((length - i) / n)
+
+        result.push(arr.slice(i, i + groupSize))
     }
 
     return result
